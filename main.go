fe2e872628f6f37052e601c539a4e441143dbdcf
zishuo.liu <henry.yuhua.liu@gmail.com>
2025-06-26 09:21:42 -0400

    add main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	port := ":8080"
+	fmt.Printf("Starting server at http://localhost%s\n", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		fmt.Printf("Server failed: %s\n", err)
+	}
+}
